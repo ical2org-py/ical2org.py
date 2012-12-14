d5a8e0729784dfb74a9116e4b540210c0dab8524
Aitor Soroa <a.soroa@gmail.com>
2012-12-14 22:53:44 +0100

    first version, seems to work

--- ical2org.py
+++ ical2org.py
@@ -1,12 +1,14 @@
+from math import floor
 from datetime import date, datetime, timedelta
-from dateutil.relativedelta import relativedelta
 from icalendar import Calendar, Event
+
 
 def canonical_date(d):
     # d can be date or datetime
     try:
         return d.replace(tzinfo=None)
     except TypeError:
+        # d is date. Convert to datetime.
         dord = d.replace().toordinal()
         return datetime.fromordinal(dord)
 
@@ -14,22 +16,25 @@ def orgDate(dt):
     '''given a datetime, return YYYY-MM-DD DayofWeek HH:MM'''
     return dt.strftime("<%Y-%m-%d %a %H:%M>")
 
-def recurrent_dates(rec_start, rec_end, rec_str, interval_start, interval_end):
+REC_DELTAS = { 'YEARLY' : 365,
+               'WEEKLY' :  7,
+               'DAILY'  :  1 }
+
+def recurring_date(rec_start, rec_end, rec_str, interval_start, interval_end):
     
     result = []
-    rec_deltas = { "YEARLY", 365,
-                   "WEEKLY",  7,
-                   "DAILY",  1)
     rec_duration = rec_end - rec_start
-    delta_ord = (interval_start.toordinal() - rec_start.toordinal()) / rec_deltas[rec_str].toordinal()
-    delta = timedelta(days=ceiling(delta_ord))
+    rec_days = REC_DELTAS[rec_str]
+    delta_ord = (interval_start.toordinal() - rec_start.toordinal()) / rec_days
+    date_aux = rec_start + timedelta(days = rec_days * int(delta_ord))
+    delta = timedelta(days = rec_days)
     end = interval_end
-    if rec_end < interval_end: end = rec_end
-    date_aux = rec_start
-    while True:
+    if rec_end > interval_start and rec_end < interval_end: end = rec_end
+    while date_aux < interval_start:
         date_aux += delta
-        if date_aux > rec_end: break
+    while date_aux < end:
         result.append( (date_aux, date_aux + rec_duration) )
+        date_aux += delta
     return result
 
 def inBetween(comp, start, end):
@@ -39,10 +44,12 @@ def inBetween(comp, start, end):
     comp_end=canonical_date(comp['DTEND'].dt)
     if 'RRULE' in comp:
         # recurring event. Return true unless the event it's over
-        if 'UNTIL' not in comp['RRULE']: return [ (comp_start, comp_end) ]
-        # check wether finish time has passed
-        finish_date = canonical_date(comp['RRULE']['UNTIL'][0])
+        if 'UNTIL' in comp['RRULE']:
+            finish_date = canonical_date(comp['RRULE']['UNTIL'][0])
+        else :
+            finish_date = end
         if finish_date < start: return []
+        return recurring_date(comp_start, comp_end, comp['RRULE']['FREQ'][0], start, finish_date)
     if comp_start > end: return []
     if comp_end < start: return []
     return [ (comp_start, comp_end) ]
@@ -51,10 +58,9 @@ cal = Calendar.from_ical(open('basic.ics','rb').read())
 #cal = Calendar.from_ical(open('kk.ics','rb').read())
 
 now=datetime.now()
-start = now - relativedelta( months = +1)
-end = now + relativedelta( months = +1)
+start = now - timedelta( days = +30)
+end = now + timedelta( days = +30)
 for comp in cal.walk():
     for comp_start, comp_end in inBetween(comp, start, end):
         print("* {}".format(comp['SUMMARY'].to_ical()))
         print("{}--{}".format(orgDate(comp_start), orgDate(comp_end)))
-
