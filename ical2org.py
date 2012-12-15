0747478b4fb582226221109c042a6d995b938582
Aitor Soroa <a.soroa@gmail.com>
2012-12-15 18:19:48 +0100

    Take timezones into account

--- ical2org.py
+++ ical2org.py
@@ -2,28 +2,85 @@
 
 import sys
 from math import floor
-from datetime import date, datetime, timedelta
+from datetime import date, datetime, timedelta, tzinfo
 from icalendar import Calendar, Event
 
 REC_DELTAS = { 'YEARLY' : 365,
                'WEEKLY' :  7,
                'DAILY'  :  1 }
 
+# These tzinfo classes are taken from python documentation
+
+ZERO = timedelta(0)
+HOUR = timedelta(hours=1)
+
+# A UTC class.
+
+class UTC_tz(tzinfo):
+    """UTC"""
+
+    def utcoffset(self, dt):
+        return ZERO
+
+    def tzname(self, dt):
+        return "UTC"
+
+    def dst(self, dt):
+        return ZERO
+
+# A class capturing the platform's idea of local time.
+
+import time as _time
+
+STDOFFSET = timedelta(seconds = -_time.timezone)
+if _time.daylight:
+    DSTOFFSET = timedelta(seconds = -_time.altzone)
+else:
+    DSTOFFSET = STDOFFSET
+
+DSTDIFF = DSTOFFSET - STDOFFSET
+
+class Local_tz(tzinfo):
+
+    def utcoffset(self, dt):
+        if self._isdst(dt):
+            return DSTOFFSET
+        else:
+            return STDOFFSET
+
+    def dst(self, dt):
+        if self._isdst(dt):
+            return DSTDIFF
+        else:
+            return ZERO
+
+    def tzname(self, dt):
+        return _time.tzname[self._isdst(dt)]
+
+    def _isdst(self, dt):
+        tt = (dt.year, dt.month, dt.day,
+              dt.hour, dt.minute, dt.second,
+              dt.weekday(), 0, 0)
+        stamp = _time.mktime(tt)
+        tt = _time.localtime(stamp)
+        return tt.tm_isdst > 0
+
 def canonical_date(d):
+    '''Given a date or a datetime, return the equivalent datetime in UTC timezone'''
     # d can be date or datetime
     try:
-        return d.replace(tzinfo=None)
-    except TypeError:
+        return d.astimezone(UTC_tz())
+    except AttributeError:
         # d is date. Convert to datetime.
-        dord = d.toordinal()
-        return datetime.fromordinal(dord)
+        new_dt = datetime(year = d.year, month = d.month, day = d.day, tzinfo = Local_tz())
+        return  new_dt.astimezone(UTC_tz())
 
 def orgDate(dt):
-    '''given a datetime, return YYYY-MM-DD DayofWeek HH:MM'''
-    return dt.strftime("<%Y-%m-%d %a %H:%M>")
+    '''given a datetime in UTC, return YYYY-MM-DD DayofWeek HH:MM using local timezone'''
+    return dt.astimezone(Local_tz()).strftime("<%Y-%m-%d %a %H:%M>")
 
 def recurring_events(rec_start, rec_end, delta_str, interval_start, interval_end):
-    
+
     result = []
     rec_duration = rec_end - rec_start
     delta_days = REC_DELTAS[delta_str]
@@ -69,9 +126,9 @@ progname, ifname = sys.argv
 
 cal = Calendar.from_ical(open(ifname,'rb').read())
 #cal = Calendar.from_ical(open('kk.ics','rb').read())
-#cal = Calendar.from_ical(open('basic.ics','rb').read())
+# cal = Calendar.from_ical(open('basic.ics','rb').read())
 
-now=datetime.now()
+now=canonical_date(datetime.now(Local_tz()))
 start = now - timedelta( days = +30)
 end = now + timedelta( days = +30)
 for comp in cal.walk():
