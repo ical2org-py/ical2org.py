5c8226c5cf4ed5bc97f76551beeaa0aa2cb7c981
Jan Vlcinsky <jan.vlcinsky@tamtamresearch.com>
2017-12-15 03:14:59 +0100

    Refactored to Convertor class.

--- ical2orgpy.py
+++ ical2orgpy.py
@@ -10,38 +10,19 @@ def print_error(msg):
     print(msg, file=sys.stderr)
 
 
-try:
-    from tzlocal import get_localzone
-    LOCAL_TZ = get_localzone()
-except ImportError as e:
-    # Change here your local timezone
-    # TODO: refactor to enter default timezone via command line option
-    tzname = "Europe/Paris"
-    msg = "Warning: Unable to import tzlocal, setting timezone %s" % tzname
-    print_error(msg)
-    LOCAL_TZ = timezone(tzname)
-
-# Window length in days (left & right from current time). Has to be positive.
-WINDOW = 90
-# leave empty if you don't want to attach any tag to recurring events
-RECUR_TAG = ":RECURRING:"
-
-# Do not change anything below
-
-
-def orgDatetime(dt):
+def orgDatetime(dt, local_tz):
     '''Timezone aware datetime to YYYY-MM-DD DayofWeek HH:MM str in localtime.
     '''
-    return dt.astimezone(LOCAL_TZ).strftime("<%Y-%m-%d %a %H:%M>")
+    return dt.astimezone(local_tz).strftime("<%Y-%m-%d %a %H:%M>")
 
 
-def orgDate(dt):
+def orgDate(dt, local_tz):
     '''Timezone aware date to YYYY-MM-DD DayofWeek in localtime.
     '''
-    return dt.astimezone(LOCAL_TZ).strftime("<%Y-%m-%d %a>")
+    return dt.astimezone(local_tz).strftime("<%Y-%m-%d %a>")
 
 
-def get_datetime(dt):
+def get_datetime(dt, local_tz):
     '''Convert date or datetime to local datetime.
     '''
     if isinstance(dt, datetime):
@@ -53,7 +34,7 @@ def get_datetime(dt):
         # timezones, so create first a utc datetime, and convert to local
         # timezone
         aux_dt = datetime(year=dt.year, month=dt.month, day=dt.day, tzinfo=utc)
-        return aux_dt.astimezone(LOCAL_TZ)
+        return aux_dt.astimezone(local_tz)
 
 
 def add_delta_dst(dt, delta):
@@ -75,37 +56,40 @@ def advance_just_before(start_dt, timeframe_start, delta_days):
         start_dt, timedelta(days=delta_days * int(delta_ord))), int(delta_ord))
 
 
-def generate_event_iterator(comp, timeframe_start, timeframe_end):
+def generate_event_iterator(comp, timeframe_start, timeframe_end, local_tz):
     '''Get iterator with the proper delta (days, weeks, etc)'''
     # Note: timeframe_start and timeframe_end are in UTC
     if comp.name != 'VEVENT':
         return []
     if 'RRULE' in comp:
         return {
-            'WEEKLY': EventRecurDaysIter(7, comp, timeframe_start,
-                                         timeframe_end),
-            'DAILY': EventRecurDaysIter(1, comp, timeframe_start,
-                                        timeframe_end),
+            'WEEKLY':
+            EventRecurDaysIter(7, comp, timeframe_start, timeframe_end,
+                               local_tz),
+            'DAILY':
+            EventRecurDaysIter(1, comp, timeframe_start, timeframe_end,
+                               local_tz),
             'MONTHLY': [],
-            'YEARLY': EventRecurYearlyIter(comp, timeframe_start,
-                                           timeframe_end)
+            'YEARLY':
+            EventRecurYearlyIter(comp, timeframe_start, timeframe_end,
+                                 local_tz)
         }[comp['RRULE']['FREQ'][0]]
     else:
-        return EventSingleIter(comp, timeframe_start, timeframe_end)
+        return EventSingleIter(comp, timeframe_start, timeframe_end, local_tz)
 
 
-class EventSingleIter:
+class EventSingleIter(object):
     '''Iterator for non-recurring single events.'''
 
-    def __init__(self, comp, timeframe_start, timeframe_end):
-        self.ev_start = get_datetime(comp['DTSTART'].dt)
+    def __init__(self, comp, timeframe_start, timeframe_end, local_tz):
+        self.ev_start = get_datetime(comp['DTSTART'].dt, local_tz)
 
         # Events with the same begin/end time same do not include
         # "DTEND".
         if "DTEND" not in comp:
             self.ev_end = self.ev_start
         else:
-            self.ev_end = get_datetime(comp['DTEND'].dt)
+            self.ev_end = get_datetime(comp['DTEND'].dt, local_tz)
 
         self.duration = self.ev_end - self.ev_start
         self.result = ()
@@ -125,12 +109,12 @@ class EventSingleIter:
         return aux
 
 
-class EventRecurDaysIter:
+class EventRecurDaysIter(object):
     '''Iterator for daily-based recurring events (daily, weekly).'''
 
-    def __init__(self, days, comp, timeframe_start, timeframe_end):
-        self.ev_start = get_datetime(comp['DTSTART'].dt)
-        self.ev_end = get_datetime(comp['DTEND'].dt)
+    def __init__(self, days, comp, timeframe_start, timeframe_end, local_tz):
+        self.ev_start = get_datetime(comp['DTSTART'].dt, local_tz)
+        self.ev_end = get_datetime(comp['DTEND'].dt, local_tz)
         self.duration = self.ev_end - self.ev_start
         self.is_count = False
         if 'COUNT' in comp['RRULE']:
@@ -142,9 +126,10 @@ class EventRecurDaysIter:
         self.delta = timedelta(delta_days)
         if 'UNTIL' in comp['RRULE']:
             if self.is_count:
-                raise "UNTIL and COUNT MUST NOT occur in the same 'recur'"
-            self.until_utc = get_datetime(
-                comp['RRULE']['UNTIL'][0]).astimezone(utc)
+                msg = "UNTIL and COUNT MUST NOT occur in the same 'recur'"
+                raise ValueError(msg)
+            self.until_utc = get_datetime(comp['RRULE']['UNTIL'][0],
+                                          local_tz).astimezone(utc)
         else:
             self.until_utc = timeframe_end
         if self.until_utc < timeframe_start:
@@ -191,22 +176,22 @@ class EventRecurDaysIter:
         return self.next_until()
 
 
-class EventRecurMonthlyIter:
+class EventRecurMonthlyIter(object):
     pass
 
 
-class EventRecurYearlyIter:
-    def __init__(self, comp, timeframe_start, timeframe_end):
-        self.ev_start = get_datetime(comp['DTSTART'].dt)
-        self.ev_end = get_datetime(comp['DTEND'].dt)
+class EventRecurYearlyIter(object):
+    def __init__(self, comp, timeframe_start, timeframe_end, local_tz):
+        self.ev_start = get_datetime(comp['DTSTART'].dt, local_tz)
+        self.ev_end = get_datetime(comp['DTEND'].dt, local_tz)
         self.start = timeframe_start
         self.end = timeframe_end
         self.is_until = False
         if 'UNTIL' in comp['RRULE']:
             self.is_until = True
             self.end = min(self.end,
-                           get_datetime(
-                               comp['RRULE']['UNTIL'][0]).astimezone(utc))
+                           get_datetime(comp['RRULE']['UNTIL'][0],
+                                        local_tz).astimezone(utc))
         if self.end < self.start:
             # Default values for no iteration
             self.i = 0
@@ -224,7 +209,8 @@ class EventRecurYearlyIter:
         self.years = range(self.start.year, self.end.year + 1)
         if 'COUNT' in comp['RRULE']:
             if self.is_until:
-                raise "UNTIL and COUNT MUST NOT occur in the same 'recur'"
+                msg = "UNTIL and COUNT MUST NOT occur in the same 'recur'"
+                raise ValueError(msg)
             self.years = range(self.ev_start.year, self.end.year + 1)
             del self.years[comp['RRULE']['COUNT'][0]:]
         self.i = 0
@@ -269,6 +255,9 @@ def main():
     if (len(sys.argv) == 1) or ("-h" in sys.argv) or ("--help" in sys.argv):
         print(main.__doc__)
         sys.exit(0)
+    # TODO: get following default values from command line options
+    default_window = 90
+    default_tz = "Europe/Paris"
 
     to_close = []
 
@@ -291,8 +280,9 @@ def main():
             sys.exit(1)
     else:
         fh_w = sys.stdout
+    convertor = Convertor(default_window, default_tz)
     try:
-        convert(fh, fh_w)
+        convertor(fh, fh_w)
         sys.exit(0)
     except IcalParsingError as e:
         print_error(e)
@@ -303,45 +293,70 @@ def main():
     sys.exit(1)
 
 
-def convert(fh, fh_w):
-    try:
-        cal = Calendar.from_ical(fh.read())
-    except ValueError as e:
-        msg = "ERROR parsing ical file: %s" % str(e)
-        raise IcalParsingError(msg)
+class Convertor(object):
+    RECUR_TAG = ":RECURRING:"
 
-    now = datetime.now(utc)
-    start = now - timedelta(days=WINDOW)
-    end = now + timedelta(days=WINDOW)
-    for comp in cal.walk():
+    # Do not change anything below
+
+    def __init__(self, window=90, default_tz="Europe/Paris"):
+        """window: Window length in days (left & right from current time). Has
+        to be positive.
+        """
+        self.local_tz = self._set_local_tz(default_tz)
+        self.window = window
+
+    def _set_local_tz(self, default_tz):
         try:
-            event_iter = generate_event_iterator(comp, start, end)
-            for comp_start, comp_end, rec_event in event_iter:
-                SUMMARY = ""
-                if "SUMMARY" in comp:
-                    SUMMARY = comp['SUMMARY'].to_ical()
-                    SUMMARY = SUMMARY.replace('\\,', ',')
-                if not len(SUMMARY):
-                    SUMMARY = "(No title)"
-                fh_w.write("* {}".format(SUMMARY))
-                if rec_event and len(RECUR_TAG):
-                    fh_w.write(" {}\n".format(RECUR_TAG))
-                fh_w.write("\n")
-                if isinstance(comp["DTSTART"].dt, datetime):
-                    fh_w.write("  {}--{}\n".format(
-                        orgDatetime(comp_start), orgDatetime(comp_end)))
-                else:  # all day event
-                    fh_w.write("  {}--{}\n".format(
-                        orgDate(comp_start),
-                        orgDate(comp_end - timedelta(days=1))))
-                if 'DESCRIPTION' in comp:
-                    DESCRIPTION = '\n'.join(
-                        comp['DESCRIPTION'].to_ical().split('\\n'))
-                    DESCRIPTION = DESCRIPTION.replace('\\,', ',')
-                    fh_w.write("{}\n".format(DESCRIPTION))
+            from tzlocal import get_localzone
+            return get_localzone()
+        except ImportError as e:
+            # Change here your local timezone
+            # TODO: refactor to enter default timezone via command line option
+            msg = "Warning: Unable to import tzlocal, setting timezone %s"
+            print_error(msg % default_tz)
+            return timezone(default_tz)
 
-                fh_w.write("\n")
-        except Exception as e:
-            msg = "Warning: an exception occured: %s" % e
-            print_error(msg)
-    exit(0)
+    def __call__(self, fh, fh_w):
+        try:
+            cal = Calendar.from_ical(fh.read())
+        except ValueError as e:
+            msg = "ERROR parsing ical file: %s" % str(e)
+            raise IcalParsingError(msg)
+
+        now = datetime.now(utc)
+        start = now - timedelta(days=self.window)
+        end = now + timedelta(days=self.window)
+        for comp in cal.walk():
+            try:
+                event_iter = generate_event_iterator(comp, start, end,
+                                                     self.local_tz)
+                for comp_start, comp_end, rec_event in event_iter:
+                    summary = ""
+                    if "SUMMARY" in comp:
+                        summary = comp['SUMMARY'].to_ical()
+                        summary = summary.replace('\\,', ',')
+                    if not summary:
+                        summary = "(No title)"
+                    fh_w.write("* {}".format(summary))
+                    if rec_event and self.RECUR_TAG:
+                        fh_w.write(" {}\n".format(self.RECUR_TAG))
+                    fh_w.write("\n")
+                    if isinstance(comp["DTSTART"].dt, datetime):
+                        fh_w.write("  {}--{}\n".format(
+                            orgDatetime(comp_start, self.local_tz),
+                            orgDatetime(comp_end, self.local_tz)))
+                    else:  # all day event
+                        fh_w.write("  {}--{}\n".format(
+                            orgDate(comp_start, self.local_tz),
+                            orgDate(
+                                comp_end - timedelta(days=1), self.local_tz)))
+                    if 'DESCRIPTION' in comp:
+                        description = '\n'.join(
+                            comp['DESCRIPTION'].to_ical().split('\\n'))
+                        description = description.replace('\\,', ',')
+                        fh_w.write("{}\n".format(description))
+
+                    fh_w.write("\n")
+            except Exception as e:
+                msg = "Warning: an exception occured: %s" % e
+                print_error(msg)
